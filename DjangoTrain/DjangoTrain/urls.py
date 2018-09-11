8dccf28f1fd13e9bb4d628bf3ae8fd27e240c7fd
AlexMono <35962752+AlexMono@users.noreply.github.com>
2018-09-11 21:49:00 +0200

    MVT

--- DjangoTrain/DjangoTrain/urls.py
+++ DjangoTrain/DjangoTrain/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('', views.home),
+    path('/about', views.about),
+    path('/contact', views.contact),
     path('admin/', admin.site.urls),
 ]
